b91cd0b7c9ddfb54cb037c5bdefe45f6ec5c5c00
António Pós-de-Mina <antonio.pos-de-mina@outlook.com>
2022-01-26 14:21:58 +0000

    Create iis.ps1

---
+++ ~/plugins/iis.ps1
@@ -0,0 +1,88 @@
+<# Performance counter
+
+get-counter -listset * | select -ExpandProperty Counter
+
+CounterSetName
+--------------
+? HTTP Service
+? HTTP Service Url Groups
+HTTP Service Request Queues
+APP_POOL_WAS
+? Microsoft FTP Service
+? IPHTTPS Global
+? IPHTTPS Session
+WAS_W3WP
+W3SVC_W3WP
+? Active Server Pages
+? ASP.NET
+? ASP.NET Applications
+? ASP.NET State Service
+#>
+try {
+    Import-Module WebAdministration -ErrorAction Stop
+
+    '<<<local:sep(9)>>>'
+    #'<<<>>>'
+    # CMK service variables
+    $cmk_service = @{
+        # Unknown
+        State = 3;
+        Name = '';
+        PerfData = '';
+        Output = '';
+    }
+    $cmk_state = 3
+    $cmk_name = ''
+    $cmk_perfdata = ''
+    $cmk_output = ''
+
+    # get AppPools e exclui os .NET
+    Get-ChildItem IIS:\AppPools -Exclude '*.NET*' | ForEach-Object {
+        $IISAppPool = $_.Name
+        $cmk_perfdata = ''
+        $i = 0
+        Get-Counter -Counter ("\HTTP Service Request Queues($IISAppPool)\*", "\APP_POOL_WAS($IISAppPool)\*", "\WAS_W3WP(*_$IISAppPool)\*", "\W3SVC_W3WP(*_$IISAppPool)\*") -ErrorAction SilentlyContinue | 
+        Select-Object -ExpandProperty CounterSamples | 
+        ForEach-Object {
+            # extract metric name only
+            $counter = (($_.Path -replace ' ', '_') -split '\\')[-1]
+            $value = $_.CookedValue
+            if ($i -gt 0) {
+                $cmk_perfdata += '|'
+            }
+            $cmk_perfdata += "$counter=$value"
+            $i++
+        }
+        $cmk_state = 0
+        if ($_.state -ne 'Started') {
+            $cmk_state = 2
+        }
+
+        $cmk_name = "IIS AppPool $IISAppPool"
+        $cmk_output = "State: $($_.state); QueueLength; $($_.queueLength); ManagedRuntimeVersion: $($_.managedRuntimeVersion);"
+        "$cmk_state`t$cmk_name`t$cmk_perfdata`t$cmk_output"
+    }
+
+
+    # -------------------------------------
+    # IIS Sites
+
+    $cmk_state = 3
+    $cmk_name = ''
+    $cmk_perfdata = '-'
+    $cmk_output = ''
+
+    Get-ChildItem -Path IIS:\Sites | ForEach-Object {
+        $cmk_state = 0
+        if ($_.state -ne 'Started') {
+            $cmk_state = 2
+        }
+        $cmk_name = "IIS Site $($_.name)"
+        $cmk_output = "ApplicationPool: $($_.applicationPool); EnabledProtocols: $($_.enabledProtocols); PhysicalPath: $($_.physicalPath); State: $($_.state);"
+        "$cmk_state`t$cmk_name`t$cmk_perfdata`t$cmk_output"
+    }
+    '<<<>>>'
+}
+catch {
+    # nothing
+}
